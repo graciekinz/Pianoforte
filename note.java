37a66c93252830d7d92c99bc4b1947692b05e7d0
graciekinz <graciekinz166@gmail.com>
2026-01-02 15:36:19 -0800

    Add notes

--- note.java
+++ note.java
@@ -1,8 +1,33 @@
+enum MusicNote {
+  C, Dflat, D, Eflat, E, F, Gflat, G, Aflat, A, Bflat, B;
+}
+
 public class Note() {
-  // variables?
+  private int octave;
+  MusicNote x;
+
+  // sets note by default to middle C
+  public Note() {
+    x = MusicNote.C;
+    octave = 4;
+  }
   
-  // default constructor
-  // public Note() { 
-  // }
+  public Note(MusicNote x, int octave) { 
+    this.x = x;
+    this.octave = octave;
+  }
+
+  // play a note at specified octave, otherwise rest
+  public void play(int octave) {
+    switch (x) {
+      case C:
+        // play note at octave
+        break;
+      
+      default:
+        // rest
+        break;
+    }
+  }
   
 }
