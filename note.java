fc181ac051810b8a6c5c71fc2db0fce08292c2de
graciekinz <graciekinz166@gmail.com>
2026-01-02 15:44:51 -0800

    add accessor methods

--- note.java
+++ note.java
@@ -6,18 +6,29 @@ public class Note() {
   private int octave;
   MusicNote x;
 
-  // sets note by default to middle C
+  // default constructor to initialize to middle C
   public Note() {
     x = MusicNote.C;
     octave = 4;
   }
-  
+
+  // constructor to initialize variables
   public Note(MusicNote x, int octave) { 
     this.x = x;
     this.octave = octave;
   }
 
-  // play a note at specified octave, otherwise rest
+  // accessor method to get octave
+  public int getOctave() {
+    return octave;
+  }
+
+  // accessor method to get note name
+  public MusicNote getNote() {
+    return x;
+  }
+
+  // method to play a note at specified octave, otherwise rest
   public void play(int octave) {
     switch (x) {
       case C:
